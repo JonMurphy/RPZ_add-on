53a4c3df10b8d9938cdfb166db83536de1ba0cfe
Jon Murphy <jon.murphy@ipfire.org>
2024-09-27 13:22:24 -0500

    update to RPZ add-on

--- config/rpz/rpz.en.pl
+++ config/rpz/rpz.en.pl
@@ -1,3 +1,44 @@
+#  Added for Response Policy Zone (RPZ)
 %tr = (%tr,
-'rpz' => 'Response Policy Zone',
+'rpz' => 'Response Policy Zone (RPZ)',
+'rpz domain' => 'Domain',
+'rpz ip statistics' => 'IP Statistics',
+'rpz ip address' => 'IP Address',
+'rpz list' => 'RPZ lists',
+'rpz number of blocked domains' => 'Number of Blocked Domains',
+'rpz number of requests' => 'Number of blocked requests',
+'rpz total number of blocked requests' => 'Total number of blocked requests',
+'rpz percentage' => 'Percentage',
+'rpz statistics' => 'RPZ statistics',
+'rpz top 10 blocked domains' => 'Top 10 of blocked domains',
+'rpz top 10 client ips' => 'Top 10 of client IPs',
+'rpz total blocked bytes' => 'Blocked bytes',
+'rpz unbound logs' => 'RPZ log viewer',
 );
+
+# Zone files
+#
+# Name
+# URL
+# Remark
+# Action
+#
+# Add
+# Update
+# Save
+#
+# Custom lists
+# Custom allowlist
+# Custom blocklist
+# Allowed domains (one per line), infotext
+# Blocked domains (one per line), infotext
+# Enable custom allowlist:
+# Enable custom blocklist:
+#
+# Edit zonefiles entry
+# Name: *
+# URL: *
+# Remark:
+# * Required field
+# Save
+# Back
